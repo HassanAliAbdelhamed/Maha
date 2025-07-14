04266eb1daaec2a76ca462645b96f278c53d7395
unknown <zxxswe12@gmail.com>
2025-07-14 23:36:44 +0300

    رسالة توضح التغييرات اللي عملتها

---
+++ index.js
@@ -0,0 +1,47 @@
+let resName = document.getElementById("resName");
+let resBaka = document.getElementById("resBaka");
+let resTel = document.getElementById("resTel");
+let resDate = document.getElementById("resDate");
+let resTime = document.getElementById("resTime");
+
+function Submit(event){
+    event.preventDefault()
+    const message = `بعد إذن حضراتكم، عايز أعمل حجز باسم: ${resName}، بباقه: ${resBaka}، في يوم: ${resDate}، الساعة: ${resTime}، ورقم التواصل: ${resTel}`;
+    const phoneNumber = "966 55 078 1986"; // ← حط رقم الواتساب هنا بدون + ومفتاح الدولة في الأول
+    const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+
+    window.open(whatsappURL, "_blank");
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+
